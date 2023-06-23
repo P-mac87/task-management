6034c95dda2970138e6539f61646c2467ec4ed2d
p-mak87 <petmak7@hotmail.com>
2023-06-23 11:16:23 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const [tasks, setTasks] = useState([])
+const [input, setInput] = useState('')
+
+// add tasks
+  
+const handleSubmit = (e) => {
+ e.preventDefault()
+ const addTask = {
+  id: Math.floor(Math.random() * 1000),
+  text: input,
+  completed: false
+  }
+
+  setTasks([...tasks, addTask])
+  setInput('')
+}
+
+//delete tasks
+
+const deleteTask = (id) => {
+  let filteredTasks = [...tasks].filter((tasks) => tasks.id !== id)
+  setTasks(filteredTasks)
+  console.log('task deleted')
+}
+
+return (
+    <div className ='App'>
+      <div className = "container">
+       <h3>Task List   <button onClick = {handleSubmit}>+ Add Task</button></h3> 
+       
+    
+    
+    <form onSubmit = {handleSubmit}>
+     <input 
+     value = {input}
+     onChange = {e => setInput(e.target.value)}
+     placeholder ="Enter a task" 
+     type = "text"  />
+     </form>
+
+ <div>
+  {tasks.map(task => (
+    <div>
+    <p>{task.text} <button>edit</button> <button onClick = {() => deleteTask(task.id)}>delete</button></p>
+      </div>
+  ))}
+ </div>
+   
+
     </div>
+ </div>
   );
 }
 
